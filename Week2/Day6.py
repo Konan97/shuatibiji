33562baf8edfe604a05d81ae6c02ae77f6e2c186
Konan97 <sunyuting17@gmail.com>
2025-01-29 18:17:28 -0500

    Day6

---
+++ Week2/Day6.py
@@ -0,0 +1,10 @@
+# HashTable is used to check whether an element exists or not
+# 242 Valid Anagram
+
+# 349 Intersection of Two arrays
+# return list(set(nums1) & set(nums2))
+# Hash Table is not suitable for large numbers
+
+# 202 Happy Number
+# use set
+# 
