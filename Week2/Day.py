86cb0c0af104a712f51b76165f5fc1eea42a2a8e
Sun <YSUN98@volvocars.com>
2025-01-30 15:11:30 -0500

    week2 finish two pointers

--- Week2/Day.py
+++ Week2/Day.py
@@ -38,5 +38,54 @@ class Solution(object):
                 else:
                     l += 1
         return res
+    
+
+# 18 4 sum 
+# for loop then 3Sum
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] >= target and nums[i] > 0:
+                break
+            # remove duplicates
+            if i > 0 and nums[i] == nums[i-1]:
+                i += 1
+                continue
+            
+            for j in range(i+1, len(nums)):
+                # 剪枝
+                if nums[i] + nums[j] >= target and nums[i] + nums[j] > 0:
+                    break
+                # remove duplicates
+                if j > i+1 and nums[j] == nums[j-1]:
+                    j += 1
+                    continue
+                l = j + 1
+                r = len(nums) - 1
+                while l < r:
+                    if nums[i] + nums[j] + nums[l] + nums[r] == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l+1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r-1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
+                    elif nums[i] + nums[j] + nums[l] + nums[r] > target:
+                        r -= 1
+                    else:
+                        l += 1
+        return res
+
+
+            
+    
 
         
