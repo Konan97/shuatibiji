c31edba1238c02d99bcd0768bbdd2c4c2e19ae47
Konan97 <sunyuting17@gmail.com>
2025-02-02 11:52:23 -0500

    Day11

---
+++ Week2/Day11_queue_Part2.py
@@ -0,0 +1,18 @@
+# 150 Evaluate reverse Polish Notation
+# use stack
+# careful with - and /
+# return int(num2 / num1) if num2 * num1 > 0 else -(abs(num2) // abs(num1))
+
+# 239 Sliding Window Maximum
+# append queue and clear element smaller than nums[i]
+# remove queue[0] if left == queue[0]
+# append queue[0] to res
+
+# 347 Top K frequent elements
+# get freq
+# use size k min heap to sort
+# heapq return the smallest item first
+
+
+        
+        
