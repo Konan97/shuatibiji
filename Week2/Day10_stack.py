13584667b39361966d22f2f6b4961a7b8db48a50
Konan97 <sunyuting17@gmail.com>
2025-02-02 11:58:06 -0500

    Day10

--- Week2/Day10_stack.py
+++ Week2/Day10_stack.py
@@ -62,8 +62,3 @@ class MyStack(object):
 
 # 1047 Remove all adjacent duplicates in string
 # use stack to cancel out duplicates
-
-# 150 Evaluate reverse Polish Notation
-# use stack
-# careful with - and /
-# return int(num2 / num1) if num2 * num1 > 0 else -(abs(num2) // abs(num1))
