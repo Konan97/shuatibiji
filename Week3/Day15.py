f1145c45df9468b0c45c40a5aa3a8c72cf34c17e
Konan97 <sunyuting17@gmail.com>
2025-02-11 20:06:04 -0500

    day15

--- Week3/Day15.py
+++ Week3/Day15.py
@@ -85,46 +85,4 @@ class Solution(object):
         sum = left + right # mid
         return sum
 
-# 513 Find Bottom Left Tree Value
-# level traversal 
-
-# 112 Path Sum
-# more practice
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        """
-        :type root: Optional[TreeNode]
-        :type targetSum: int
-        :rtype: bool
-        """
-        if not root: return False
-        return self.traversal(root, targetSum - root.val)
-
-    def traversal(self, root, count):
-        # stop condition
-        # leave and count = 0
-        if not root.left and not root.right and count == 0:
-            return True
-        # leave
-        if not root.left and not root.right:
-            return False
-        
-        if root.left:
-            count -= root.left.val
-            if self.traversal(root.left, count):
-                return True
-            count += root.left.val
-        if root.right:
-            count -= root.right.val
-            if self.traversal(root.right, count):
-                return True
-            count += root.right.val
-        return False
-
         
