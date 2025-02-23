7c8127822eb9dc1e1af1de271e7395e0a5136631
Konan97 <sunyuting17@gmail.com>
2025-02-23 15:40:08 -0500

    day29

---
+++ Week5/Day29.py
@@ -0,0 +1,17 @@
+# 134 Gas Station
+# if total gas < total cost, cannot make it. 
+# find min accumulative sum
+# reverse loop find point to even out min accumulative sum
+
+
+# 135 Candy
+# step 1: compare right to left
+# step 2: then left to right in reverse order, take max
+
+# 860 Lemonade Change
+# 分情况讨论
+
+# 406 Queue Reconstruction by Height
+# more practice
+# sort by height and k
+# insert to queue based on k
