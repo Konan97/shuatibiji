6cfe104570ec01b7e2d50f349def9a4216bb07b6
Sun <YSUN98@volvocars.com>
2025-02-26 15:28:08 -0500

    day31

---
+++ Week5/Day31.py
@@ -0,0 +1,8 @@
+# 56 Merge Intervals
+# find the cur max right
+# if left > cur max right, then new overlapping interval
+
+
+# 738 Monotone Increasing Digits
+# if n[i] < n[i-1], n[i - 1] -= 1 make n[i] = 9 and flag i
+
