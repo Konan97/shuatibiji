274858489a2978b2c8f4dae1064f5a89802fee4b
Konan97 <sunyuting17@gmail.com>
2025-02-26 19:18:53 -0500

    day31

--- Week5/Day31.py
+++ Week5/Day31.py
@@ -6,3 +6,9 @@
 # 738 Monotone Increasing Digits
 # if n[i] < n[i-1], n[i - 1] -= 1 make n[i] = 9 and flag i
 
+# 968 Binary Tree Cameras
+# Postorder traversal 从下往上
+# 0 no cover 1 camera 2 cover
+# both covered parent no cover
+# if one is uncovered, parent camera
+# if one is camera, parent is covered
