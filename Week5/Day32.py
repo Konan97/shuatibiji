30c061797e3a10dbb23001050682311cf575f3f6
Konan97 <sunyuting17@gmail.com>
2025-03-01 16:10:44 -0500

    day33

---
+++ Week5/Day32.py
@@ -0,0 +1,13 @@
+# DP
+# 动规是由前一个状态推导出来的，而贪心是局部直接选最优的
+# 1 确定dp数组（dp table）以及下标的含义
+# 2 确定递推公式
+# 3 dp数组如何初始化
+# 4 确定遍历顺序
+# 5 举例推导dp数组
+
+# 509 Fibonacci Number
+
+# 70 Climbing Stairs
+
+# 746 Min Cost Climbing Stairs
---
+++ Week5/Day33.py
@@ -0,0 +1,9 @@
+# 62 Unique Paths
+# grid[i][j] = grid[i][j-1] + grid[i-1][j]
+
+# 63 Unique Paths II
+# obstacle remove possibilities from one side
+
+# 343 Integer Break
+# max(dp[i-j]*j, (i-j)*j, dp[i])
+
