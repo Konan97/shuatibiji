5020b232c90927d419ff470336a33d78352d04a2
Konan97 <sunyuting17@gmail.com>
2025-03-04 19:35:12 -0500

    day36

--- Week6/Day35.py
+++ Week6/Day35.py
@@ -1,10 +1,8 @@
+# 01 背包 2D
+# 01 背包 1D
+
 # 416 Partition equal subset sum
 # more practice
 # find dp[target] == target
 # for i in nums:
 #     for j in range(target, i-1, -1):
-
-
-# 1049 Last Stone Weight
-# same as 416
-# make stones reach sum/2
--- Week6/Day36.py
+++ Week6/Day36.py
@@ -1,4 +1,30 @@
+# 1049 Last Stone Weight
+# same as 416
+# make stones reach sum/2
+
+
 # 494 Target Sum
 # 找背包组合
 # more practice
 # dp[j] += dp[j-nums[i]]
+
+# 474 Ones and Zeros
+# 2D backpack of ones and zeros
+# not easy
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+                # 物品
+        for string in strs:
+            count0, count1 = 0, 0
+            for s in string:
+                if s == "1":
+                    count1 += 1
+                if s == "0":
+                    count0 += 1
+            # 背包
+            for i in range(m, count0-1, -1):
+                for j in range(n, count1-1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i-count0][j-count1] + 1)
+
+        return dp[m][n]
