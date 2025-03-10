9a038b1d1b5b9ff6334a08ae9a5955d559377084
Sun <YSUN98@volvocars.com>
2025-03-10 14:13:55 -0400

    week7

--- Week 1/Day1.py
+++
@@ -1,8 +0,0 @@
-# 704. Binary Search
-# 规范区间 [left, right]
-
-# 27. Remove Element
-# Fast, slow pointers
-
-# 977. Squares of a sorted array
-# two pointers, 中间大 两头小
---
+++ Week7/Day.py
@@ -0,0 +1,25 @@
+# 300 Longest Increasing Subsequence
+def lengthOfLIS(self, nums):
+        dp = [1 for _ in range(len(nums))]
+        
+        for i in range(len(nums)):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1) # make dp[i] bigger
+            print(dp)
+        return max(dp)
+
+# 674 Longest Continuous Increasing Subsequence
+# single layer dp
+def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1 for _ in range(len(nums))]
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                dp[i] = dp[i-1]+1
+        
+        return max(dp)
