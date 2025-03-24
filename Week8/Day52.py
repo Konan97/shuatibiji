3546b4b6aac8b02716d9a5ac297b8fdb58e80f12
Konan97 <sunyuting17@gmail.com>
2025-03-23 22:01:53 -0400

    52

---
+++ Week8/Day52.py
@@ -0,0 +1,14 @@
+# 101 area of the island
+# remove island connected to the side
+
+
+# 102 remove isolated island
+# mark connected island 
+
+
+# 103 flowing water
+# start from border to center
+# find overlapping area
+
+
+# 104 
