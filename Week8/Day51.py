f523a6540a0241df111e565b7c9092fa03b51786
Sun <YSUN98@volvocars.com>
2025-06-16 15:41:26 -0400

    Google round 2

--- Week8/Day51.py
+++ Week8/Day51.py
@@ -1,4 +1,41 @@
 # 99 number of islands (ACM)
+# 200 number of islands (LeetCode)
+# bfs <><><><><><><><><><><><><><><><><><><>
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        row, col = len(grid), len(grid[0])
+        visited = set()
+        island = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    self.bfs(grid, (i, j), visited)
+                    island += 1
+        return island
+        
+        
+    def bfs(self, grid, start, visited):
+        row, col = len(grid), len(grid[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        queue = []
+        queue.append(start)
+        while queue:
+            node = queue.pop(0)
+            X = node[0]
+            Y = node[1]
+            for d in directions:
+                newX = X + d[0]
+                newY = Y + d[1]
+                if 0 > newX or newX >= row or 0 > newY or newY >= col or grid[newX][newY] == "0" or (newX, newY) in visited:
+                    continue
+                visited.add((newX, newY))
+                queue.append((newX, newY))
+
+# dfs <><><><><><><><><><><><><><><><><><><><><><><><>
 def dfs(grid, visited, x, y):
     directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     for i, j in directions:
