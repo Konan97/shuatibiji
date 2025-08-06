f04d0bf91a72b0cd44281aa94de35efbf169f522
Sun <ysun98@volvocars.com>
2025-08-06 15:50:05 -0400

    Google

--- Week8/Day51_bfs.py
+++ Week8/Day51_bfs.py
@@ -72,6 +72,47 @@ if __name__ == '__main__':
 
 # 99 bfs 加入列队立刻标记visited
 
+# 947 Most Stones Removed with Same Row or Column
+# solving using graph connected component
+# BFS
+class Solution(object):
+    def removeStones(self, stones):
+        """
+        :type stones: List[List[int]]
+        :rtype: int
+        """
+        # BFS find connected components
+        row, col = defaultdict(list), defaultdict(list)
+
+        for i in range(len(stones)):
+
+            row[stones[i][0]].append(i)
+            col[stones[i][1]].append(i)
+
+        queue = []
+        visited = set()
+
+        connected = 0
+        for i in range(0, len(stones)):
+            if tuple(stones[i]) not in visited:
+                queue.append(stones[i])
+                visited.add(tuple(stones[i]))
+                while queue:
+                    top = queue.pop()
+                    x, y = top[0], top[1]
+                    for neighbor in row[x]:
+                        if tuple(stones[neighbor]) not in visited:
+                            queue.append(stones[neighbor])
+                            visited.add(tuple(stones[neighbor]))
+                    for neighbor in col[y]:
+                        if tuple(stones[neighbor]) not in visited:
+                            queue.append(stones[neighbor])
+                            visited.add(tuple(stones[neighbor]))
+                connected += 1
+
+
+        return len(stones) - connected
+    
 # 100 max area of island
 
 # 286 Walls and Gates
